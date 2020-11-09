4e843b2ecffcaefaa21a686d7b48a2db7fc62a5f
Geet <rgeet13@gmail.com>
2020-11-09 22:19:46 +0530

    Done

--- scripts/post-install.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra')
-
-try {
-    fs.copySync('./config/config.env.example', './config/config.env')
-    console.log('Environment file created successfully.')
-} catch (err) {
-    console.error(err)
-}
